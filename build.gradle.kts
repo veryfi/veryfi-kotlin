bf3bfe486449defecd8e0b288b08e5a03fb123e6
Alejandro Maldonado <alejandromaldonado@veryfi.com>
2025-07-01 13:26:54 -0500

    LP-1214: Update gradle and publishing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.1"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.0"
-	kotlin("plugin.spring") version "1.6.0"
-	id("org.jetbrains.dokka") version "1.6.0"
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm") version "2.2.0"
+	kotlin("plugin.spring") version "2.2.0"
+	id("org.jetbrains.dokka") version "2.0.0"
 	jacoco
 	id("maven-publish")
+	id("org.jreleaser") version "1.19.0"
+	signing
 }
 
+signing {
+    val signingKey = System.getenv("JRELEASER_GPG_SECRET_KEY")
+    val signingPassword = System.getenv("JRELEASER_GPG_PASSPHRASE")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+	sign(publishing.publications)
+}
 
 publishing {
 	publications {
 		create<MavenPublication>("mavenJava") {
 			groupId = "com.veryfi"
 			artifactId = "veryfi-kotlin"
-			version = "1.1.0"
+			version = "2.0.0"
 			from(components["java"])
 			pom {
 				name.set("Veyfi Kotlin")
@@ -43,14 +51,46 @@ publishing {
 	}
 	repositories {
 		maven {
-			name = "OSSRH";
-			setUrl("https://s01.oss.sonatype.org/content/repositories/releases")
-			credentials {
-				username = System.getenv("MAVEN_USERNAME")
-				password = System.getenv("MAVEN_PASSWORD")
-			}
-		}
+            setUrl(layout.buildDirectory.dir("staging-deploy"))
+        }
 	}
+}
+
+jreleaser {
+	release {
+        github {
+            enabled = true
+			skipRelease = true
+			skipTag = true
+        }
+    }
+	project {
+		name = "Veyfi Kotlin"
+		version = "2.0.0"
+		description = "Android kotlin module for communicating with the Veryfi OCR API"
+		copyright = "Veryfi"
+		java {
+			groupId = "com.veryfi"
+			artifactId = "veryfi-kotlin"
+		}
+    }
+	deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    setActive("ALWAYS")
+					sign = false
+                    url = "https://central.sonatype.com/api/v1/publisher"
+					stagingRepository(layout.buildDirectory.dir("staging-deploy").get().toString())
+					setAuthorization("Basic")
+                }
+            }
+        }
+    }
+}
+
+tasks.named("jreleaserDeploy") {
+    dependsOn("publish")
 }
 
 repositories {
@@ -58,7 +98,7 @@ repositories {
 }
 
 jacoco {
-	toolVersion = "0.8.7"
+	toolVersion = "0.8.13"
 	reportsDirectory.set(layout.buildDirectory.dir("jacocoReports"))
 }
 
@@ -74,15 +114,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.json:json:20180813")
-	testImplementation("junit:junit:4.13.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+	implementation("org.json:json:20250517")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.1")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+    	exclude(group = "com.vaadin.external.google", module ="android-json")
 	}
 }
 

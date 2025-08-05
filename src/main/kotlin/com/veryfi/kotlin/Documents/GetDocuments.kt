03be00785c76539a0adf452cee872293655122fb
Alejandro Maldonado <alejandromaldonado@veryfi.com>
2025-08-05 16:34:28 -0500

    LP-1235: Add request parameters to getDocuments

--- src/main/kotlin/com/veryfi/kotlin/Documents/GetDocuments.kt
+++ src/main/kotlin/com/veryfi/kotlin/Documents/GetDocuments.kt
@@ -8,22 +8,34 @@ import java.util.concurrent.CompletableFuture
 /**
  * Get JSON of documents. https://docs.veryfi.com/api/receipts-invoices/search-documents/
  * Returns a json string [String] with the list of documents.
+ * @param parameters Additional request parameters
  * @return the url [String]
  */
-fun Client.getDocuments(): String {
+fun Client.getDocuments(parameters: JSONObject? = null): String {
     val endpointName = "/documents/"
     val requestArguments = JSONObject()
+    if (parameters != null && !parameters.isEmpty) {
+        for (key in JSONObject.getNames(parameters)) {
+            requestArguments.put(key, parameters[key])
+        }
+    }
     return request(HttpMethod.GET, endpointName, requestArguments)
 }
 
 /**
  * Get JSON of documents. https://docs.veryfi.com/api/receipts-invoices/search-documents/
  * Returns a json string list of documents.
+ * @param parameters Additional request parameters
  * @return the list of previously processed documents [String]
  */
-fun Client.getDocumentsAsync(): CompletableFuture<String> {
+fun Client.getDocumentsAsync(parameters: JSONObject? = null): CompletableFuture<String> {
     val endpointName = "/documents/"
     val requestArguments = JSONObject()
+    if (parameters != null && !parameters.isEmpty) {
+        for (key in JSONObject.getNames(parameters)) {
+            requestArguments.put(key, parameters[key])
+        }
+    }
     return requestAsync(HttpMethod.GET, endpointName, requestArguments)
 }
 
